af005b503214de8b4386db975a7dd8346862cf32
Gustavo <gustavo-u2186@hotmail.com>
2025-06-28 09:20:05 -0400

    replace css values for tailwind class

--- src/components/SingleTask.vue
+++ src/components/SingleTask.vue
@@ -36,7 +36,7 @@ const handleRemoveRequest = ():void => {
 
 <template>
   <div
-    class="SingleTask text-[#222] w-full bg-primary-dark p-2 mb-20 mt-10 rounded-xl shadow-md transform hover:shadow-lg transition duration-300 ease-in-out"
+    class="SingleTask text-gray-800 w-full bg-primary-dark p-2 mb-20 mt-10 rounded-xl shadow-md transform hover:shadow-lg transition duration-300 ease-in-out"
   >
     <ElementHeader
     @remove="handleRemoveRequest"
