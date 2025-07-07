2251654f72c58abd3cb5521e2e336cfcd4ae9f22
Gustavo <gustavo-u2186@hotmail.com>
2025-07-07 18:58:25 -0400

    fix extra spaces

--- src/stores/useTaskBoardStore.ts
+++ src/stores/useTaskBoardStore.ts
@@ -3,58 +3,58 @@ import { ref, computed } from 'vue'
 
 export const useTaskBoard = defineStore('TaskBoard', () => {
 
-  type Task = {
+type Task = {
   id: string
   title: string
   assignee: string
   comment: string
 }
-  type Column = {
+type Column = {
     id: string
     title: string
   }
 
 
-  const columns = ref<Column[]>([])
-  const tasks = ref<Record<string, Task[]>>({})
+const columns = ref<Column[]>([])
+const tasks = ref<Record<string, Task[]>>({})
 
-  const getColumns = computed(() => columns.value)
+const getColumns = computed(() => columns.value)
 
-  const addColumn = (column: Column): void => {
-    columns.value = [...columns.value, column]
-  }
+const addColumn = (column: Column): void => {
+  columns.value = [...columns.value, column]
+}
 
-  const removeColumn = (columnId: string): void => {
-    const newColumns = columns.value.filter((column) => column.id !== columnId)
-    columns.value = newColumns
+const removeColumn = (columnId: string): void => {
+  const newColumns = columns.value.filter((column) => column.id !== columnId)
+  columns.value = newColumns
 
-     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { [columnId]: _, ...rest } = tasks.value
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { [columnId]: _, ...rest } = tasks.value
     tasks.value = { ...rest }
   }
 
-  const addTask = ({ columnId, task }: { columnId: string; task: Task }): void => {
-    const newColumnTasks = [...(tasks.value[columnId] || []), task]
-    tasks.value = { ...tasks.value, [columnId]: newColumnTasks }
+const addTask = ({ columnId, task }: { columnId: string; task: Task }): void => {
+  const newColumnTasks = [...(tasks.value[columnId] || []), task]
+  tasks.value = { ...tasks.value, [columnId]: newColumnTasks }
   }
 
-  const removeTask = ({columnId,task}: { columnId: string; task: Task }): void => {
-    const newColumnsTasks = tasks.value[columnId].filter(taskItem => taskItem.id !== task.id)
-    tasks.value = { ...tasks.value, [columnId]: newColumnsTasks }
+const removeTask = ({columnId,task}: { columnId: string; task: Task }): void => {
+  const newColumnsTasks = tasks.value[columnId].filter(taskItem => taskItem.id !== task.id)
+  tasks.value = { ...tasks.value, [columnId]: newColumnsTasks }
   }
 
-  
 
 
 
-  return {
-    columns,
-    getColumns,
-    addColumn,
-    removeColumn,
-    addTask,
-    tasks,
-    removeTask
-  }
+
+return {
+  columns,
+  getColumns,
+  addColumn,
+  removeColumn,
+  addTask,
+  tasks,
+  removeTask
+}
 
 })
